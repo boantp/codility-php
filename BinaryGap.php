0978ef1de22bc5b7bb4334064aca45309951f9db
Boan Tua Pasaribu <boantuapasaribu@gmail.com>
2018-09-28 17:26:55 +0700

    Update BinaryGap.php

--- BinaryGap.php
+++ BinaryGap.php
@@ -1,85 +1,51 @@
 <?php
-// you can write to stdout for debugging purposes, e.g.
-// print "this is a debug messagen";
+/**
+***A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N
+=> number 9 has binary representation 1001 and contains one binary gap of length 2
+=> number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
+=> number 20  has binary representation 10100 and contains one binary gap of length 1
+=> number 15 has binary representation 1111 and has no binary gaps.
+=> number 32 has binary representation 100000 and has no binary gaps
+***Write a function: that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+**/
 
-$N = 1041;
-function solution($N) {
-    // write your code in PHP7.0
-    //convert into binary number
-  $n_bin = decbin($N);
-  //echo $n_bin."<br />";
-    //$parse_n = explode("0", $n_bin);
-  $parse_n = str_split($n_bin);
-    //var_dump($parse_n);
-  $hold_one = 1;
-  $hold_zero = 1;
-  $before_n = null;
-  $array_hold_zero = [];
-  foreach($parse_n as $k => $v) {
-    if($v == "1") {         
-      if($before_n == "1"){
-       $hold_one = $hold_one + 1;
-       if(sizeof($parse_n) - 1 == $k)
-       {
-         $array_hold_one[] = $hold_one;
-         if(sizeof($array_hold_one) == 1) {
-                                                                              //unset($array_hold_zero);
-          $array_hold_one = array();
-        }
-      }
-    }
-    if($before_n == "0"){
-     $array_hold_zero[] = $hold_zero;
-     if(sizeof($parse_n) - 1 != $k){
-      $hold_zero = 1;
-    }
-  }
+/**
+ * Check Interger for longest binary gap
+ *
+ * @param integer $n value that we want to check for binary gap
+ * 
+ * @throws Some_Exception_Class If something interesting cannot happen
+ * @author Boant TP <https://github.com/boantp/>
+ * @return Integer longest binary gap or 0
+ */  
+function solution($n) {
+	$binary = decbin($n);
+	//zero that surrounded by one at both ends, always start by 1 and end with one that binary gap
+	$binary_in_arr = str_split($binary);
 
-} elseif($v == "0") {
-  if($before_n == "0"){
-   $hold_zero = $hold_zero + 1;
-   if(sizeof($parse_n) - 1 == $k)
-   {
-     $array_hold_zero[] = $hold_zero;
-     if(sizeof($array_hold_zero) == 1) {
-                                                                              //unset($array_hold_zero);
-      $array_hold_zero = array();
-    }
-  }
-}
-if($before_n == "1"){
- $array_hold_one[] = $hold_one;
- if(sizeof($parse_n) - 1 != $k){
-  $hold_one = 1;
-}
-}
-}
-$before_n = $v;
-}
-arsort($array_hold_zero);
-arsort($array_hold_one);
-$array_hold_zero = array_values($array_hold_zero);
-$array_hold_one = array_values($array_hold_one);
-$zero = isset($array_hold_zero[0]) ? (int)$array_hold_zero[0] : null;
-$one = isset($array_hold_one[0]) ? (int)$array_hold_one[0] : null;
-if($zero > $one){
-  if($zero == 1 || $zero == null) {
-    return 0;
-  } else {
-    return $zero;
-  }
+	if(! in_array(0, $binary_in_arr)) {
+		return 0;
+	}
 
-} else 
-{
-  if($one == 1 || $one == null) {
-    return 0;
-  } else {
-    return $one;
-  }
+	$gap 		= NULL;
+	$last_key 	= 0;
+	foreach ($binary_in_arr as $key => $value) {
+		if($value == 1 && $key != 0) {
+			$gap[] = ($key - $last_key) - 1;//5
+			$last_key = $key;//5 - 0, 9 - 5
+		}
+	}
+
+	if($gap == NULL) {
+		return 0;
+	} else {
+		$gap_int = max($gap);
+	}
+
+	return $gap_int;
 }
 
-  //var_dump($array_hold_zero, $array_hold_one);
-}
+$n = 1041;
+var_dump(solution($n)); 
 
-echo solution($N);
 ?>
